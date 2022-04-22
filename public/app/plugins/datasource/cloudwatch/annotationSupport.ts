67d42958d8855e6950e93df1940b0bc1b6ae691a
Erik Sundell <erik.sundell87@gmail.com>
2022-04-22 11:02:35 +0200

    add unit tests

--- public/app/plugins/datasource/cloudwatch/annotationSupport.ts
+++ public/app/plugins/datasource/cloudwatch/annotationSupport.ts
@@ -1,9 +1,10 @@
 import { AnnotationQuery } from '@grafana/data';
 import { AnnotationQueryEditor } from './components/AnnotationQueryEditor';
 import { isCloudWatchAnnotation } from './guards';
-import { CloudWatchAnnotationQuery, LegacyAnnotationQuery } from './types';
+import { CloudWatchAnnotationQuery, CloudWatchQuery, LegacyAnnotationQuery } from './types';
 
 export const CloudWatchAnnotationSupport = {
+  // converts legacy angular style queries to new format. Also sets the same default values as what used to be set in the deprecated angular directive
   prepareAnnotation: (
     query: LegacyAnnotationQuery | AnnotationQuery<CloudWatchAnnotationQuery>
   ): AnnotationQuery<CloudWatchAnnotationQuery> => {
@@ -21,10 +22,37 @@ export const CloudWatchAnnotationSupport = {
       target: {
         ...query.target,
         ...query,
+        statistic: query.statistic || 'Average',
+        region: query.region || 'default',
         queryMode: 'Annotations',
-        refId: query.refId ?? 'annotationQuery',
+        refId: query.refId || 'annotationQuery',
       },
     };
+  },
+  // return undefined if query is not complete so that annotation query execution is quietly skipped
+  prepareQuery: (anno: AnnotationQuery<CloudWatchAnnotationQuery>): CloudWatchQuery | undefined => {
+    if (!anno.target) {
+      return undefined;
+    }
+
+    const {
+      prefixMatching,
+      actionPrefix,
+      alarmNamePrefix,
+      statistic,
+      namespace,
+      metricName,
+      dimensions = {},
+    } = anno.target;
+    const validPrefixMatchingQuery = !!prefixMatching && !!actionPrefix && !!alarmNamePrefix;
+    const validMetricStatQuery =
+      !prefixMatching && !!namespace && !!metricName && !!statistic && !!Object.values(dimensions).length;
+
+    if (validPrefixMatchingQuery || validMetricStatQuery) {
+      return anno.target;
+    }
+
+    return undefined;
   },
   QueryEditor: AnnotationQueryEditor,
 };
